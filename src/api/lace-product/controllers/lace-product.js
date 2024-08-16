9cdfe5ae5795fd39d823ab70a7f2a601134559ac
Akshay kumar singh <akshaysing975@gmail.com>
2024-08-16 22:55:21 +0530

    first commit

---
+++ src/api/lace-product/controllers/lace-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lace-product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::lace-product.lace-product');
---
+++ src/api/lace-product/routes/lace-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lace-product router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::lace-product.lace-product');
